845ab30c827022ca25e970c6ab52a978eacee578
BoXing2017 <34951785+BoXing2017@users.noreply.github.com>
2019-01-14 17:04:37 +0800

    Update loadReviewModel.py

--- loadReviewModel.py
+++ loadReviewModel.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: Akhil Gupta
-# @Email:  akhilgupta.official@gmail.com  
-# @Github username: @codeorbit
-# @Last Modified by:   Akhil Gupta
-# @Last Modified time: 2016-07-1
+
 
 import redis
 from bottle import route, run
